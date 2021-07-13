0d7a626c37d532a021d191002c7d2403a7dfecee
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-07-13 13:58:06 -0300

    Centralizar todos os contextos em um arquivo só

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header/'
 import Spinner from './components/Spinner'
 import CategoriesContext from './contexts/CategoriesContext'
-import FilterContext from './contexts/FilterContext'
 import LoadingContext from './contexts/LoadingContext'
 import MessageContext from './contexts/MessageContext'
-import useLoading from './hooks/useLoading'
 import Routes from './routes/Routes'
 import CategoriesService from './services/CategoriesService'
 import { GlobalStyle } from './styles/GlobalStyle'
 
 function App() {
-  const [filter, setFilter] = useState('')
-  const [message, setMessage] = useState('')
-  const [categories, setCategories] = useState({})
-  const [addRequest, removeRequest, isLoading] = useLoading()
+  const { setMessage } = useContext(MessageContext)
+  const { setCategories } = useContext(CategoriesContext)
 
+  const { addRequest, removeRequest } = useContext(LoadingContext)
   // eslint-disable-next-line
   useEffect(() => loadCategories(), [])
 
@@ -30,22 +27,16 @@ function App() {
   }
 
   return (
-    <FilterContext.Provider value={{ filter, setFilter }}>
-      <LoadingContext.Provider value={{ addRequest, removeRequest, isLoading }}>
-        <MessageContext.Provider value={{ message, setMessage }}>
-          <CategoriesContext.Provider value={{ categories }}>
-            <Spinner></Spinner>
-            <div className="page-container">
-              <Header></Header>
+    <>
+      <Spinner />
+      <div className="page-container">
+        <Header />
 
-              <GlobalStyle />
-              <Routes />
-            </div>
-            <Footer></Footer>
-          </CategoriesContext.Provider>
-        </MessageContext.Provider>
-      </LoadingContext.Provider>
-    </FilterContext.Provider>
+        <GlobalStyle />
+        <Routes />
+      </div>
+      <Footer />
+    </>
   )
 }
 
